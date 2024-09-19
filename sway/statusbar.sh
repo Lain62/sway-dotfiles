d9523519dd7eead73b4289d138c24d504dfde4ea
lain62 <laimadelain@gmail.com>
2024-09-19 11:15:40 +0700

    arf arf

---
+++ sway/statusbar.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+while :; 
+do
+	uptime_formatted=$(uptime | cut -d ',' -f1 | cut -d ' ' -f4,5)
+	date_formatted=$(date "+%a %F")
+	clock_formatted=$(date "+%H:%M")
+	battery_status=$(cat /sys/class/power_supply/BAT1/status)
+	battery_info=$(upower --show-info $(upower --enumerate |\
+grep 'BAT') |\
+egrep "percentage" |\
+awk '{print $2}')
+	echo Uptime $uptime_formatted \| Battery $battery_info \| $date_formatted \| $clock_formatted
+	sleep 10
+done
+
